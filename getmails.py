6795ce2bb08a4635184cf005ee44c98f0b86146a
Jorge-Rodriguez <jorge.rodriguez@kampiki.com>
2015-05-18 15:10:36 +0300

    Initial commit

---
+++ getmails.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""Retrieve a list of all active email addresses on FORGE LDAP database
+
+Usage:
+    getmails.py <username> <password>
+    getmails.py <username> -p
+    getmails.py -h | --help
+
+Options:
+    -h --help           Show this screen.
+    -p --pass           Interactive password
+"""
+import ldap
+import getpass
+from docopt import docopt
+
+
+def _getMailList(user, passwd):
+    conn = ldap.initialize('ldaps://auth.forgeservicelab.fi')
+    conn.bind_s('cn=%s,ou=accounts,dc=forgeservicelab,dc=fi' % user, passwd)
+
+    return [mail for maillist in
+            c.search_s('ou=accounts,dc=forgeservicelab,dc=fi',
+                       ldap.SCOPE_ONELEVEL,
+                       filterstr='(!(|(employeeType=hidden)(employeeType=disabled)(cn=test*)(sn=service)))',
+                       attrlist=['mail'])
+            for mail in maillist]
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    if args['--pass']:
+        args['<password>'] = getpass.getpass()
+
+    _getMailList(args['<username>'], args['<password>'])
